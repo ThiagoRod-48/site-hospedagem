718875ed726880e57360ddafbe7a4fe2849299ba
ThiagoRod-48 <thiago.rodrigo73@yahoo>
2025-12-30 18:40:44 -0300

    Eu sinto que agora vai

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,7 +8,10 @@ import Login from "./pages/Login";
 import Place from "./pages/Place";
 import Register from "./pages/Register";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000/api"
+    : "https://soarbnb.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 function App() {
